8545cec01eea6314076fa1af27ab033735294d7f
Sergio Cecarini <sergio@cecarini.com>
2018-04-19 19:48:05 +0100

    pack

--- Package.swift
+++ Package.swift
@@ -2,6 +2,9 @@ import PackageDescription
 
 let package = Package(
     name: "TaskQueue",
+    products: [
+        .library(name: "TaskQueue", targets: ["TaskQueue"]),
+    ],
     targets: [],
     dependencies: [
         
